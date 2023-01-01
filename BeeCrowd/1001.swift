ec431196741e321abf76eb44551d380cdcfb422e
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-01-01 20:06:26 -0300

    1001

---
+++ BeeCrowd/1001.swift
@@ -0,0 +1,18 @@
+//
+//  1001.swift
+//  BeeCrowd
+//
+//  Created by Lucas Barbosa de Oliveira on 01/01/23.
+//
+
+import Foundation
+//
+//var a = Int(readLine()!)!
+//var b = Int(readLine()!)!
+//
+//func soma(num1: Int,num2: Int) -> Int{
+//    var c = a + b
+//    return c
+//}
+//
+//print(soma(num1: a, num2: b))
